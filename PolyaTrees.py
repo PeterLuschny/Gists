989220f00223adb2c8cc97d6cd1cdf456aa7df41
PeterLuschny <peter.luschny@gmail.com>
2024-09-10 06:06:08 +0200

    dach

--- PolyaTrees.py
+++ PolyaTrees.py
@@ -1,5 +1,7 @@
+# Using Python 3.12.4
+
 import time
-from typing import Callable
+from typing import Callable, Generator
 
 class Timer:
     def __init__(
@@ -30,12 +32,12 @@ class Timer:
 
 def Benchmark(T: Callable[[int, int], int]) -> list[float]:
     B: list[float] = []
-    for s in [16, 32, 64, 128, 256]:
+    for s in [16, 32, 64, 128, 256, 512]:
         t = Timer(str(s))
         t.start()
         [[T(n, k) for k in range(n + 1)] for n in range(s)]
         B.append(t.stop())
-    return [B[1]/B[0], B[2]/B[1], B[3]/B[2], B[4]/B[3]]
+    return [B[i + 1] / B[i] for i in range(5)]
 
 
 """ Polya Trees
@@ -51,11 +53,10 @@ def Benchmark(T: Callable[[int, int], int]) -> list[float]:
     [9] [0, 0, 1, 22, 98, 191, 252, 278, 285, 286]
 """
 
-from functools import cache, lru_cache
+from functools import cache
 
 # polyatree ------------------------------------------
 # https://codegolf.stackexchange.com/questions/275413/
-
 
 @cache
 def polya_tree(n: int, k: int) -> int:
@@ -75,36 +76,69 @@ def polya_tree(n: int, k: int) -> int:
     if n == 1: return 0 if k == 0 else 1
 
     def W(n: int, k: int, u: int, w: int) -> int: 
-       q, r = divmod(u, w)
-       if r != 0: return 0
-       return q * polya_tree(k, n) * polya_tree(q, n - 1)
+        q, r = divmod(u, w)
+        if r != 0: return 0
+        return q * polya_tree(k, n) * polya_tree(q, n - 1)
 
     return sum(sum(W(k, i, n - i, j) 
            for i in range(1, n)) for j in range(1, n)) // (n - 1)
 
 
-# tree_count ------------------------------------------------
+# tree_count ----------------------------------------------------------
 # Jonathan Allan, https://codegolf.stackexchange.com/a/275420
 
+@cache
+def divisors(n: int) -> Generator[int, None, None]:
+    return (d for d in range(n, 0, -1) if n % d == 0)
 
-@lru_cache(maxsize=None)
+@cache
 def tree_count(nodes: int, max_height: int) -> int:
     if nodes == 1:
         return int(max_height > 0)
-    return sum(sum(tree_count(i, max_height) * (m := (nodes - i) // d)
-                    * tree_count(m, max_height - 1)
-                    for d in range(1, nodes - i + 1)
-                    if (nodes - i) % d == 0
-                  ) for i in range(1, nodes)
-              ) // (nodes - 1)
+
+    next_height = max_height - 1
+    return sum(
+        sum(
+            tree_count(i, max_height) * d * tree_count(d, next_height)
+            for d in divisors(nodes - i)
+        )
+        for i in range(1, nodes)
+    ) // (nodes - 1)
 
 
-# Results  ------------------------------------
+# TreeCount -----------------------------------------------------------
+# gsitcia
+
+@cache
+def Divisors(n: int) -> Generator[int, None, None]:
+    return (d for d in range(n, 0, -1) if n % d == 0)
+
+@cache
+def TreeCount(nodes: int, max_height: int) -> int:
+    if nodes == 1:
+        return int(max_height > 0)
+    if max_height == 0:
+        return 0
+
+    next_height = max_height - 1
+    return sum(
+        TreeCount(i, max_height) * TreeCount_1(nodes - i, next_height)
+        for i in range(1, nodes)
+    ) // (nodes - 1)
+
+
+@cache
+def TreeCount_1(nodes_i: int, next_height: int) -> int:
+    return sum(d * TreeCount(d, next_height) for d in Divisors(nodes_i))
+
+
+# Results  ----------------------------------------
 
 if __name__ == "__main__":
 
-    print("polya_tree", Benchmark(polya_tree))
+    # print("polya_tree", Benchmark(polya_tree))
     print("tree_count", Benchmark(tree_count))
+    print("TreeCount ", Benchmark(TreeCount))
 
     """
                16 0.0085 sec
@@ -115,11 +149,21 @@ if __name__ == "__main__":
 
     polya_tree [16.8, 10.9, 15.6, 16.1]
 
-               16 0.0072 sec
-               32 0.0844 sec
-               64 0.8911 sec
-              128 10.8109 sec
-              256 140.7037 sec
+               16 0.0014 sec
+               32 0.0080 sec
+               64 0.0659 sec
+              128 0.4913 sec
+              256 3.7559 sec
+              512 31.3221 sec
 
-    tree_count [11.7, 10.6, 12.1, 13.0]
+    tree_count [5.7, 8.2, 7.4, 7.6, 8.3]
+
+               16 0.0016 sec
+               32 0.0089 sec
+               64 0.0672 sec
+              128 0.5700 sec
+              256 4.6278 sec
+              512 44.6065 sec
+
+    TreeCount  [5.6, 7.5, 8.5, 8.1, 9.6]
     """
