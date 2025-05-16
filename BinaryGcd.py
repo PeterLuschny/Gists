041e822dfd3d29e6d6b12d7941a53b273a8a4dfa
PeterLuschny <peter.luschny@gmail.com>
2025-05-16 15:50:34 +0200

    jyu

--- BinaryGcd.py
+++ BinaryGcd.py
@@ -45,14 +45,15 @@ def gcd(a: int, b: int) -> int:
         # print(a, b)
         count += 1
 
-        d = abs(a - b) # Difference 'd' will be even (odd - odd = even).
+        # Difference 'd' will be odd (odd - odd = even).
+        d = a - b if a > b else b - a
 
         # Calculate CTZ of d for the next iteration's 'a'.
         # (d & -d) isolates LSB of abs(d) correctly for 2's complement.
         dz = (d & -d).bit_length() - 1 
 
         # The new 'b' for the next iteration is min of the current odd 'a' and odd 'b'.
-        b = min(a, b) 
+        b = a if a < b else b
 
         # The new 'a' for the next iteration is abs(d) with removed trailing zeros from d to make it odd.
         a = d >> dz
@@ -65,6 +66,7 @@ def gcd(a: int, b: int) -> int:
 
 if __name__ == "__main__":
     import math
+    from timeit import default_timer as timer
     
     for n in range(13): 
         print([gcd(n, k) for k in range(n + 1)])
@@ -123,4 +125,71 @@ if __name__ == "__main__":
         for k in range(n + 1): gcd(n, k)
         return count
 
-    # print([n*math.log(n)/A383441(n) for n in range(1000,2000)])
+    print([A383441(n) for n in range(62)])
+
+    """
+    The following implementation is from Rosetta Code:
+    https://rosettacode.org/wiki/Greatest_common_divisor#Iterative_binary_algorithm_8
+
+    It faithfully implements Algorithm B given by Don Knuth in TAOCP, volume 2, section 4.5.2, page 321 of the second edition.
+    """
+
+    def gcd_bin(u: int, v: int) -> int:
+        global count
+    
+        u, v = abs(u), abs(v) # u >= 0, v >= 0
+        if u < v:
+            u, v = v, u # u >= v >= 0
+        if v == 0:
+            return u
+   
+        # u >= v > 0
+        k = 1
+        while u & 1 == 0 and v & 1 == 0: # u, v - even
+            u >>= 1; v >>= 1
+            k <<= 1
+       
+        t = -v if u & 1 else u
+        while t:
+            count += 1
+            while t & 1 == 0:
+                t >>= 1
+            if t > 0:
+                u = t
+            else:
+                v = -t
+            t = u - v
+        
+        return u * k
+
+    """
+    So lets' compare the number of iterations needed using the two implementations."""
+
+    def B383441(n: int) -> int:
+        global count
+        count = 0
+        for k in range(n + 1): gcd_bin(n, k)
+        return count
+
+    print([B383441(n) for n in range(62)])
+
+    """
+    The number is always smaller in our implementation than in the Rosetta Code implementation of Knuth's representation.
+    """
+
+    start = timer()
+    [A383441(n) for n in range(1000)]
+    end = timer()
+    print(end - start) 
+ 
+    start = timer()
+    [B383441(n) for n in range(1000)]
+    end = timer()
+    print(end - start)
+
+    """ And the computing time is also smaller! 
+
+    For n = 10000 the time was:
+    284.94
+    374.55
+    """
