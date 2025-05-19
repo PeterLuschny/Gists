ebb801080d959ebdd4ab12203b807d759282352c
PeterLuschny <peter.luschny@gmail.com>
2025-05-19 13:51:11 +0200

    qpd

--- BinaryGcd.py
+++ BinaryGcd.py
@@ -1,8 +1,7 @@
-
-# The algorithm binary GCD of two nonnegative numbers 
+# The algorithm binary GCD of two nonnegative numbers
 # * is described in TAOCP, volume 2, section 4.5.2, page 321 of the 2nd edition,
 # * is also known as Stein's algorithm,
-# * is based on the fact that the GCD of two numbers is the same as the GCD of 
+# * is based on the fact that the GCD of two numbers is the same as the GCD of
 #   their difference and the smaller number.
 
 # Knuth describes the advantages of the algorithm:
@@ -10,8 +9,17 @@
 # (i) subtraction, (ii) testing whether a number is even or odd, and (iii) shifting
 # the binary representation of an even number to the right."
 
-# The global variable 'count' records the number of iteration executed in GCD. 
+# Our goal is to compare this algorithm, as described by Knuth, 
+# with a variant that can be found in various places on the Internet. 
+# For example, it is described by Sergey Slotin et al. on Algorithmica.org. 
+# https://en.algorithmica.org/hpc/algorithms/gcd/ where it says: 
+# "The main optimization ideas belong to Daniel Lemire and Ralph Corderoy, 
+# who had nothing better to do on the Christmas holidays of 2013."
+# (We have no way to verify this.)
+
+# The global variable 'count' records the number of iteration executed in GCD.
 count = 0
+
 
 def gcd(a: int, b: int, trac: bool = False) -> int:
     """Compute the greatest common divisor (GCD) using the binary GCD algorithm."""
@@ -31,20 +39,40 @@ def gcd(a: int, b: int, trac: bool = False) -> int:
         return a
     if a == b:
         return b
-    
-    # Reduce to save same unnecessary iterations.
-    if a > b and b > (a >> 1): 
+
+    if trac:
+        print(a, b)
+
+    # Gives gcd(a, b) and gcd(a, a - b) the same number of iterations
+    # if a > b and saves some iterations.
+    if a > b and b > (a >> 1):
         b = a - b
-    elif b > a and a > (b >> 1): 
+    elif b > a and a > (b >> 1):
         a = b - a
+
+    # If you look at the triangle gcd(n, k) for 0 <= k <= n, (OEIS A109004)
+    # you will see that the rows are symmetric in the sense that
+    # gcd(n, k) = gcd(n, n - k). 
+    # It's very counterintuitive when analyzing Stein's algorithm as described 
+    # by Knuth that the number of iterations for the right-hand value is larger, 
+    # often many times larger, than the number of iterations required for the 
+    # left-hand value. One can also understand this as a very natural optimization 
+    # being forgotten. We address this with the above assignment.
 
     # CTZ - Count Trailing Zeros of x.
     # Counting trailing zeros is relevant because the binary GCD algorithm
-    # removes all factors of 2 from the numbers by shifting their binary 
-    # representation to the right. 
+    # removes all factors of 2 from the numbers by shifting their binary
+    # representation to the right.
     # (x & -x) isolates the lowest set bit.
     # .bit_length() gives the number of bits needed to represent that isolated bit.
     # -1 Subtracting 1 converts this to the count of trailing zeros.
+    
+    # This quantity is used below in the form 
+    # a = a >> ((a & -a).bit_length() - 1). 
+    # It's not immediately obvious to a common man what this bit sh** and shiffting
+    # means, but it's quite trivial. It's the function odd := n -> n*2^(-ordp(n, 2)),
+    # where ordp(n, 2) is the 2-adic order of n, or, to put it in plain English, 
+    # it is the odd part of n (OEIS A000265).
 
     # az = CTZ of a.
     az = (a & -a).bit_length() - 1
@@ -60,12 +88,13 @@ def gcd(a: int, b: int, trac: bool = False) -> int:
 
     # Same with b. b is now the odd part of the original b.
     b >>= bz
-   
+
     # Loop invariant: 'a' and 'b' are always odd.
     # The loop ends when 'a' and 'b' are equal.
     while b != a:
 
-        if trac: print(a, b)
+        if trac:
+            print(a, b)
         count += 1
 
         # Difference 'd' will be odd (odd - odd = even).
@@ -75,11 +104,11 @@ def gcd(a: int, b: int, trac: bool = False) -> int:
         # (d & -d) isolates LSB of abs(a - b).
         dz = (d & -d).bit_length() - 1
 
-        # The new 'b' for the next iteration is min of 
+        # The new 'b' for the next iteration is min of
         # the current odd 'a' and odd 'b'.
         b = a if a < b else b
 
-        # The new 'a' for the next iteration is d with 
+        # The new 'a' for the next iteration is d with
         # removed trailing zeros to make it odd.
         a = d >> dz
 
@@ -89,13 +118,75 @@ def gcd(a: int, b: int, trac: bool = False) -> int:
     return a << shift
 
 
+"""
+    Algorithm B as given by Don Knuth in TAOCP, volume 2, section 4.5.2, 
+    page 321 of the second edition.
+"""
+
+
+def gcd_taocp(u: int, v: int, trac: bool = False) -> int:
+    global count
+
+    # Handle negative inputs for correctness.
+    if u < 0:
+        u = -u
+    if v < 0:
+        v = -v
+
+    # Handle the corner cases.
+    if u == 0:
+        return v
+    if v == 0:
+        return u
+    if u == v:
+        return v
+
+    if trac:
+        print(u, v)
+
+    # B1 Find power of 2.
+    k = 0
+    while u & 1 == 0 and v & 1 == 0:
+        k += 1
+        u >>= 1
+        v >>= 1
+
+    # B2 Initialize
+    if u & 1:  # u - odd
+        t = -v
+    else:
+        t = u
+
+    # Main loop
+    while t != 0:
+        count += 1
+        if trac:
+            print(u, v)
+
+        # B3 Is t odd?
+        while t & 1 == 0:
+            t >>= 1
+
+        if t > 0:
+            u = t
+        else:
+            v = -t
+
+        # B4 Subtract
+        t = u - v
+
+    return u << k
+
+
 # === Test ==========================================
 
 if __name__ == "__main__":
 
     import math
     from timeit import default_timer as timer
-    
+
+    print("\nThe gcd(n, k) for 0 <= k <= n. In the OEIS A109004.\n")
+
     for n in range(13):
         print([gcd(n, k) for k in range(n + 1)])
 
@@ -127,11 +218,11 @@ if __name__ == "__main__":
     if OK:
         print("All tests passed.")
 
-    s = """\nA383441(n) is the number of iterations needed in the 
-    binary GCD algorithm to compute gcd(n, k) for k from 0 to n (using 
-    the above implementation). The corresponding gcd's are in A109004. 
-    [Edit: Meanwhile we optimized the implementation a bit so that the number
-    of iterations here is even less than the counts given in A383441.]
+    s = """\nA383441(n) is the number of iterations needed in the
+    binary GCD algorithm to compute gcd(n, k) for 0 <= k <= n, 
+    using our implementation. The corresponding gcd's are in A109004. 
+    [Meanwhile we optimized the implementation a bit so that the number
+    of iterations here is slightly less than the counts given in A383441.] 
     The sequence starts at n = 0 and the first 62 terms of A383441 are:\n"""
     print(s)
 
@@ -145,64 +236,47 @@ if __name__ == "__main__":
     print([A383441(n) for n in range(62)])
 
     """
-    The following implementation is from Rosetta Code:
-    https://rosettacode.org/wiki/Greatest_common_divisor#Iterative_binary_algorithm_8
-
-    It claims to implement Algorithm B given by Don Knuth in TAOCP, 
-    volume 2, section 4.5.2, page 321 of the second edition.
+    Let's compare the number of iterations of the two implementations.
     """
 
-    def gcd_bin(u: int, v: int, trac: bool = False) -> int:
-        global count
-
-        u, v = abs(u), abs(v)  # u >= 0, v >= 0
-        if u < v:
-            u, v = v, u  # u >= v >= 0
-        if v == 0:
-            return u
-
-        # u >= v > 0
-        k = 1
-        while u & 1 == 0 and v & 1 == 0:  # u, v - even
-            u >>= 1
-            v >>= 1
-            k <<= 1
-
-        t = -v if u & 1 else u
-
-        while t:
-        
-            if trac: print(u, v)
-            count += 1
-
-            while t & 1 == 0:
-                t >>= 1
-            if t > 0:
-                u = t
-            else:
-                v = -t
-            t = u - v
-
-        return u * k
-
-    """
-    So let's compare the number of iterations needed using the two implementations."""
+    print("\nThe number of iterations in Knuth's TAOCP implementation is:")
 
     def B383441(n: int) -> int:
         global count
         count = 0
         for k in range(n + 1):
-            gcd_bin(n, k)
+            gcd_taocp(n, k)
         return count
 
     print([B383441(n) for n in range(62)])
-    
+    print()
+
     """
     [0, 0, 0, 2, 0, 4, 4, 12, 2, 8, 10, 22, 10, 28, 24, 30, 10, 32, 22, 42, 24, 38, 46, 68, 26, 58, 58, 68, 54, 94, 62, 102, 34, 68, 76, 74, 52, 116, 94, 112, 62, 120, 84, 146, 102, 122, 140, 166, 70, 154, 126, 154, 128, 188, 144, 182, 124, 178, 192, 222, 138, 244]
     
-    [0, 1, 2, 5, 5, 10, 11, 19, 13, 20, 22, 34, 25, 42, 41, 46, 33, 55, 44, 66, 51, 63, 73, 93, 59, 88, 90, 99, 90, 125, 99, 135, 81, 113, 118, 121, 100, 160, 141, 157, 117, 171, 136, 195, 159, 173, 197, 221, 138, 212, 188, 215, 196, 250, 211, 243, 200, 244, 264, 288, 217, 312]
+    [0, 0, 1, 4, 4, 9, 10, 18, 12, 19, 21, 33, 24, 41, 40, 45, 32, 54, 43, 65, 50, 62, 72, 92, 58, 87, 89, 98, 89, 124, 98, 134, 80, 112, 117, 120, 99, 159, 140, 156, 116, 170, 135, 194, 158, 172, 196, 220, 137, 211, 187, 214, 195, 249, 210, 242, 199, 243, 263, 287, 216, 311]
     """
-    
+
+    print(
+        "\nComparing the number of iterations in the two implementations side by side."
+    )
+    print("| n | #A383441(n) | #TAOCP(n) | #TAOCP(n) - #A383441(n) |")
+    for n in range(33):
+        print(f"{n} | {A383441(n)} | {B383441(n)} | {B383441(n) - A383441(n)} |")
+
+    print(
+        "\nThe number of iterations in our implementation is always smaller."
+    )
+
+    """ To trace the computations call the functions with 
+    a third parameter set to 'True'. """
+
+    print("\nTracing the gcd of 40902 and 24140 in our version:")
+    gcd(40902, 24140, True)
+    print("\nTracing the gcd of 40902 and 24140 in Knuth's TAOCP:")
+    gcd_taocp(40902, 24140, True)
+    print()
+
     def AK383441(n: int) -> list[int]:
         global count
         L = []
@@ -212,38 +286,53 @@ if __name__ == "__main__":
             L.append(count)
         return L
 
-    for n in range(13): print(AK383441(n))
-    
+    print(
+        "The number of iterations in our implementation is symmetric in the sense\n gcd(max(a, b), min(a, b)) = gcd(max(a, b), max(a, b) - min(a, b)).\n"
+    )
+
+    for n in range(17):
+        print([n], AK383441(n))
+
     def BK383441(n: int) -> list[int]:
         global count
         L = []
         for k in range(n + 1):
             count = 0
-            gcd_bin(n, k)
+            gcd_taocp(n, k)
             L.append(count)
         return L
 
-    for n in range(13): print(BK383441(n))
+    print(
+        "\nThe number of iterations in the TAOCP setup is not symmetric in the above sense.\n"
+    )
 
-    # for n in range(50): print([n], A383441(n), B383441(n))
+    for n in range(17):
+        print([n], BK383441(n))
 
-    """ To trace the computation call the function with 
-    a third parameter 'True'. """
-    
-    print("\nThe gcd of 40902 and 24140 is:")
-    gcd(40902, 24140, True)
-    print("\nThe gcd of 40902 and 24140 is:")
-    gcd_bin(40902, 24140, True)
+    print("\nTiming the two implementations.\n")
+
+    for L in [500, 1000]:
+        start = timer()
+        [A383441(n) for n in range(L)]
+        end = timer()
+        print(
+            f"Elapsed time for {L} rows is {(end - start):2.2f} seconds [our version]."
+        )
+
+        start = timer()
+        [B383441(n) for n in range(L)]
+        end = timer()
+        print(f"Elapsed time for {L} rows is {(end - start):2.2f} seconds [TAOCP].\n")
+
+    """ Timing the two implementations.
+    Elapsed time for 500 rows is 0.34 seconds [our version].
+    Elapsed time for 500 rows is 0.48 seconds [TAOCP].
+
+    Elapsed time for 1000 rows is 1.55 seconds [our version].
+    Elapsed time for 1000 rows is 2.20 seconds [TAOCP].
+
+    Elapsed time for 2000 rows is 6.93 seconds [our version].
+    Elapsed time for 2000 rows is 9.62 seconds [TAOCP].
+    """
+
     print()
-
-    s = """\nThe number of iterations in our implementation is always smaller than in the Rosetta Code implementation of Knuth's presentation. Maybe this is a bug in the Rosetta Code implementation or just another variant.\n"""
-    print(s)
-
-    start = timer()
-    [A383441(n) for n in range(1000)]
-    end = timer()
-    L = 1000
-    print(f"Elapsed time for {L} rows is {end - start} seconds.\n")
-
-    """ Time for n =  1000 :   1.53 seconds."""
-    """ Time for n = 10000 : 284.94 seconds."""
